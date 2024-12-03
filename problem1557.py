36399e3dd294e66d113b5345d5089b4bfc6ffc70
andre-maia51 <acmc.0410@gmail.com>
2024-12-02 21:20:10 -0300

    feat: comentei melhor o meu problema de SCC

--- problem1557.py
+++ problem1557.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 
 class Solution:
+    # Aqui ta rolando o algoritmo que roda o DFS no grafo original e reverso
     def calculateSCC(self, n, edges):
         graph = defaultdict(list)
         reverse_graph = defaultdict(list)
@@ -38,16 +39,23 @@ class Solution:
                 dfs_transposed(node, reverse_graph, visited, scc)
                 sccs.append(scc)
 
+        # Ta retornando um array com todos os SCCs que tem no grafo
         return sccs
 
+
+    # Função principal
     def findSmallestSetOfVertices(self, n, edges):
         sccs = self.calculateSCC(n, edges)
 
+        # Aqui eu to criando um dicionário que separa cada item do array de
+        # SCCs de acordo com um índice
         scc_index = {}
         for idx, scc in enumerate(sccs):
             for node in scc:
                 scc_index[node] = idx
 
+        # Aqui, to verificando se existe alguma aresta entre um nó de um SCC 
+        # para um nó de outro SCC
         scc_indegree = defaultdict(int)
         for u, v in edges:
             u_scc = scc_index[u]
